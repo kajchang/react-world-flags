463f01b68ae5786c3efb1d08919d2ef2781bf5b4
Kai Chang <kaijchang@gmail.com>
2023-07-07 05:19:55 -0700

    allow ioc code

--- src/country.js
+++ src/country.js
@@ -3,7 +3,7 @@ import countries from 'world-countries'
 export const getAlphaTwoCode = code => {
   const uc = String(code).toUpperCase()
   const country = countries.find(
-    c => c.cca2 === uc || c.ccn3 === uc || c.cca3 == uc
+    c => c.cca2 === uc || c.ccn3 === uc || c.cca3 == uc || c.cioc == uc
   )
   return (country && country.cca2) || code
 }
